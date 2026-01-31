6d433606cd83691dd2449c916d0979ec97e81516
Aleksa Janjic <aleksajanjic@hotmail.com>
2026-01-31 18:51:01 +0100

    update

--- app/app/Enums/UserRole.php
+++ app/app/Enums/UserRole.php
@@ -5,6 +5,6 @@ namespace App\Enums;
 enum UserRole: string
 {
     case ADMIN = 'admin';
-    case MODERATOR = 'aoderator';
-    case MEMBER = 'aember';
+    case MODERATOR = 'moderator';
+    case MEMBER = 'member';
 }
