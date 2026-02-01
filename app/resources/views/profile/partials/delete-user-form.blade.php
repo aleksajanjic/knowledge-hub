335471511abc53a9ac44024ec3f3ef43e56e96a1
Aleksa Janjic <aleksajanjic@hotmail.com>
2026-02-01 17:42:11 +0100

    wip

--- app/resources/views/profile/partials/delete-user-form.blade.php
+++ app/resources/views/profile/partials/delete-user-form.blade.php
@@ -14,7 +14,7 @@
         x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
     >{{ __('Delete Account') }}</x-buttons.danger-button>
 
-    <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
+    <x-layout.modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
             @csrf
             @method('delete')
@@ -51,5 +51,5 @@
                 </x-buttons.danger-button>
             </div>
         </form>
-    </x-modal>
+    </x-layout.modal>
 </section>
