776240eb2d10499c4b43ba9e561e68b61595f0d1
Aleksa Janjic <aleksajanjic@hotmail.com>
2026-02-01 19:37:55 +0100

    wip

--- app/app/Http/Controllers/UserProfileController.php
+++ app/app/Http/Controllers/UserProfileController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Answer;
+use App\Models\Question;
 use App\Models\User;
+use App\Models\Vote;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -26,8 +29,7 @@ class UserProfileController extends Controller
             'questions_asked' => $user->questions_count,
             'answers_given' => $user->answers_count,
             'accepted_answers' => $user->answers()->where('is_accepted', true)->count(),
-            'total_upvotes' => '10'
-            // 'total_upvotes' => $this->getTotalUpvotes($user),
+            'total_upvotes' => $this->getTotalUpvotes($user),
         ];
 
         return view('profile.show', compact(
@@ -39,22 +41,18 @@ class UserProfileController extends Controller
     /**
      * Get total upvotes received by user
      */
-    // private function getTotalUpvotes(User $user): int
-    // {
-    //     $questionUpvotes = \DB::table('votes')
-    //         ->join('questions', 'votes.votable_id', '=', 'questions.id')
-    //         ->where('votes.votable_type', 'App\Models\Question')
-    //         ->where('questions.user_id', $user->id)
-    //         ->where('votes.vote_type', 'upvote')
-    //         ->count();
-    //
-    //     $answerUpvotes = \DB::table('votes')
-    //         ->join('answers', 'votes.votable_id', '=', 'answers.id')
-    //         ->where('votes.votable_type', 'App\Models\Answer')
-    //         ->where('answers.user_id', $user->id)
-    //         ->where('votes.vote_type', 'upvote')
-    //         ->count();
-    //
-    //     return $questionUpvotes + $answerUpvotes;
-    // }
+    private function getTotalUpvotes(User $user): int
+    {
+        $questionUpvotes = Vote::where('votable_type', 'Question')
+            ->where('vote', 1)
+            ->whereIn('votable_id', Question::where('user_id', $user->id)->pluck('id'))
+            ->count();
+
+        $answerUpvotes = Vote::where('votable_type', "Answer")
+            ->where('vote', 1)
+            ->whereIn('votable_id', Answer::where('user_id', $user->id)->pluck('id'))
+            ->count();
+
+        return $questionUpvotes + $answerUpvotes;
+    }
 }
